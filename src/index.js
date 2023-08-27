ae9ba4de9354e8627477372d5c08a3d4daeba51a
fmpavon <francisco.mpavon@gmail.com>
2023-08-27 19:02:36 +0200

    webapp name, logos, favicon...

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import { Preview, Editor } from './App';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
